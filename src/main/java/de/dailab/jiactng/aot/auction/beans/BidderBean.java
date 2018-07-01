a43d3f704e6ed14db08489340b5f947f138fb044
Filip Twardzik <f.twardzik@campus.tu-berlin.de>
2018-07-01 23:36:33 +0200

    message handling, stubs for bid calculating methods, basic logging

--- src/main/java/de/dailab/jiactng/aot/auction/beans/BidderBean.java
+++ src/main/java/de/dailab/jiactng/aot/auction/beans/BidderBean.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
 
+import de.dailab.jiactng.agentcore.comm.CommunicationAddressFactory;
+import de.dailab.jiactng.agentcore.comm.ICommunicationAddress;
+import de.dailab.jiactng.agentcore.comm.IGroupAddress;
+import de.dailab.jiactng.aot.auction.onto.*;
 import org.sercho.masp.space.event.SpaceEvent;
 import org.sercho.masp.space.event.SpaceObserver;
 import org.sercho.masp.space.event.WriteCallEvent;
@@ -13,22 +17,25 @@ import de.dailab.jiactng.agentcore.comm.message.JiacMessage;
 import de.dailab.jiactng.agentcore.knowledge.IFact;
 import de.dailab.jiactng.agentcore.ontology.IActionDescription;
 
-
-import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * TODO Implement this class.
  */
 public class BidderBean extends AbstractAgentBean {
 
-	/*
-	 * TODO
-	 * add properties for e.g. the multicast message group, or the bidderID
-	 * add getter methods for those properties so they can be set in the
-	 * Spring configuration file
-	 */
+    /***********************
+     * ATTRIBUTES
+     ***********************/
 	private String bidderId;
 	private String messageGroup;
+
+	private Wallet wallet;
+    private List<Resource> stack;
+
+    /***********************
+     * GETTERS & SETTERS
+     ***********************/
 
 	public String getBidderId() {
 		return this.bidderId;
@@ -45,74 +52,157 @@ public class BidderBean extends AbstractAgentBean {
 	public void setMessageGroup(String messageGroup) {
 		this.messageGroup = messageGroup;
 	}
-	/*
-	 * TODO
-	 * when the agent starts, use the action ICommunicationBean.ACTION_JOIN_GROUP
-	 * to join the multicast message group "de.dailab.jiactng.aot.auction"
-	 * for the final competition, or a group of your choosing for testing
-	 * make sure to use the same message group as the auctioneer!
-	 */
+
+    /***********************
+     * INITIALIZE AGENT
+     ***********************/
+
 	@Override
     public void doStart() throws Exception {
-
+        IGroupAddress group = CommunicationAddressFactory.createGroupAddress("test-group");
         IActionDescription joinAction = retrieveAction(ICommunicationBean.ACTION_JOIN_GROUP);
+        invoke( joinAction, new Serializable[] {group});
         this.memory.attach(new MessageObserver(), new JiacMessage());
-        log( "INFO", "Agent started.");
-
+        log.info( "Agent "+bidderId+" started.");
     }
 
-    /*
-	 * TODO
-	 * when the agent starts, create a message observer and attach it to the
-	 * agent's memory. that message observer should then handle the different
-	 * messages and send a suitable Bid in reply. see the readme and the
-	 * sequence diagram for the expected order of messages.
-	 */
+    /***********************
+     * MESSAGE OBSERVER
+     ***********************/
 
     @SuppressWarnings("serial")
     class MessageObserver implements SpaceObserver<IFact> {
-
         @SuppressWarnings("rawtypes")
         @Override
         public void notify(SpaceEvent<? extends IFact> event) {
-            // check the type of the event
+
             if (event instanceof WriteCallEvent) {
-                log("INFO","New message came!");
-                // we know it's a message due to the template, but we have to check the content
+
                 JiacMessage message = (JiacMessage) ((WriteCallEvent) event).getObject();
-                /*
-                if (message.getPayload() instanceof OfferFact) {
+                IFact payload = message.getPayload();
 
-                    OfferFact offerFact = (OfferFact) message.getPayload();
-                    log("Receiving offer " + offerFact);
+                log.info(payload.toString());
 
-                    // compare to best offer and update if lower
-                    if (bestOffer == null || offerFact.getOffer() < bestOffer.getOffer()) {
-                        bestOffer = offerFact;
-                        log("New best offer " + bestOffer);
-                    }
-                }
-                */
-                // once handled, the message should be removed from memory
+                if( payload instanceof StartAuction) handleStartAuction(message);
+                if( payload instanceof CallForBids ) handleCallForBids(message);
+                if( payload instanceof InitializeBidder ) handleInitializeBidder(message);
+                if( payload instanceof InformBuy ) handleInformBuy(message);
+                if( payload instanceof InformSell ) handleInformSell(message);
+                if( payload instanceof EndAuction ) handleEndAuction(message);
+
+                log.info(wallet.toString());
+
                 memory.remove(message);
             }
         }
     }
 
+    /****************************
+     * SEND MESSAGE TO AUCTIONEER
+     ****************************/
 
-	/*
-	 * TODO You will receive your initial "Wallet" from the auctioneer, but
-	 * afterwards you will have to keep track of your spendings and acquisitions
-	 * yourself. The Auctioneer will do so, as well.
-	 */
-
-
-	/*
-	 * our custom  helper functions
-	 */
-
-    private void log(String type, String msg) {
-        String time = LocalDateTime.now().toString();
-        System.out.println("["+time+"]["+this.bidderId + "]["+type+"]: " + msg);
+    private void send(IFact payload, ICommunicationAddress address) {
+        JiacMessage message = new JiacMessage(payload);
+        IActionDescription sendAction = retrieveAction(ICommunicationBean.ACTION_SEND);
+        invoke(sendAction, new Serializable[]{message, address});
     }
+
+    /****************************
+     * MESSAGE-SPECIFIC HANDLERS
+     ****************************/
+
+    private void handleStartAuction(JiacMessage message) {
+        send( new Register(bidderId), message.getSender() );
+    }
+
+    private void handleInitializeBidder(JiacMessage message) {
+        this.wallet = ((InitializeBidder) message.getPayload()).getWallet();
+        this.stack = ((InitializeBidder) message.getPayload()).getItems();
+    }
+
+    private void handleCallForBids(JiacMessage message) {
+        Resource resource = ((CallForBids) message.getPayload()).getResource();
+        Integer minOffer = ((CallForBids) message.getPayload()).getMinOffer();
+        Calculator calculator = new Calculator(bidderId, resource, minOffer, stack);
+        Integer myOffer = calculator.estimateOffer();
+        send( new Bid( bidderId, ((CallForBids) message.getPayload()).getCallId(), myOffer),  message.getSender() );
+    }
+
+    private void handleInformBuy(JiacMessage message) {
+        InformBuy payload = (InformBuy) message.getPayload();
+        if(payload.getType() == InformBuy.BuyType.WON) {
+            log.info("Won "+payload.getResource().toString()+" for "+payload.getPrice());
+            wallet.updateCredits(-payload.getPrice());
+            wallet.add(payload.getResource(), 1);
+        }
+    }
+
+    private void handleInformSell(JiacMessage message) {
+
+    }
+
+    private void handleEndAuction(JiacMessage message) {
+        EndAuction payload = (EndAuction) message.getPayload();
+        if(payload.getWinner()==bidderId) log.info("I WON THE AUCTION!!!!!");
+    }
+
+    /****************************
+     * CLASS FOR BID CALCULATIONS
+     ****************************/
+
+    private class  Calculator {
+
+        private String bidderId;
+        private Integer minOffer;
+        Resource resource;
+        private List<Resource> stack;
+
+        public Calculator(String bidderId, Resource resource , Integer minOffer, List<Resource> stack) {
+            this.bidderId = bidderId;
+            this.resource = resource;
+            this.minOffer = minOffer;
+            this.stack = stack;
+        }
+
+        /************************************************
+         * CHOOSE METHOD BASED ON AGENT NAME (bidder.xml)
+         ************************************************/
+
+        public Integer estimateOffer() {
+            switch(bidderId) {
+                case "20_uniform": return uniformDistribution();
+                case "20_avgBid": return avgBid();
+                case "20_maxProfit": return maxProfit();
+                case "20_maxEnd": return maxEnd();
+                default: return  uniformDistribution();
+            }
+        }
+
+        /************************************
+         * SPECIFICAL BID CALCULATING METHODS
+         ************************************/
+
+        private Integer uniformDistribution() {
+            //TODO
+            return 1;
+        }
+
+        private Integer avgBid() {
+            //TODO
+            return 2;
+        }
+
+        private Integer maxProfit() {
+            //TODO
+            return 3;
+        }
+
+        private Integer maxEnd() {
+            //TODO
+            return 4;
+        }
+
+    }
+
+
 }
